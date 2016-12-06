dcec47dec35fbf0eb5ffb34697dbd7a64376252c
Yin Ming <yinming2008@live.com>
2016-12-06 17:25:56 +0800

    DLXJ-EX-22.4

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 10 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#10 SMP PREEMPT Sun Dec 4 20:10:16 CST 2016"
+#define LINUX_COMPILE_BY "hasee"
+#define LINUX_COMPILE_HOST "hasee-QTJ5"
+#define LINUX_COMPILER "gcc version 4.9.1 20140529 (prerelease) (crosstool-NG linaro-1.13.1+bzr2672 - Linaro GCC 2014.05 - LaboDJ - Cortex-A15) "
